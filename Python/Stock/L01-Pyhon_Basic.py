20fb4e99d83b62f024128cac55e78a84ea471350
Claire Tsai <57058281+awhirl420@users.noreply.github.com>
2019-12-29 16:24:33 +0800

    Update L01-Pyhon_Basic.py

--- Python/Stock/L01-Pyhon_Basic.py
+++ Python/Stock/L01-Pyhon_Basic.py
@@ -1,3 +1,20 @@
 # take TW stock market as example
-# Fee: stock price* fee rate(1.425/1000) for buy/sell
+# Fee: stock price* fee rate(1.425/1000)* (1-discount off) for buy/sell
 # Tax: stock price* tax rate(3/1000) for sell
+# Board lot: 1000 shares
+
+# Variable Name
+account = 1000000
+stock_price = 50
+buy_amount = 1
+fee_ratio	= 1.425/1000
+tax_ratio = 3/1000
+stock_value = stock_price * buy_amount * 1000
+
+# Buy stock, and then sell the stock when price raise 40%
+account = account - stock_value * (1 + fee_ratio)
+stock_price = stock_price * 1.4
+stock_value = stock_price * buy_amount * 1000
+account = account + stock_value * (1 - fee_ratio - tax_ratio)
+
+
