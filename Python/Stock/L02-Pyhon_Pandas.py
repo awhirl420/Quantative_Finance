d61d0e939918e2c2298535a6851ea55c7bd2fe9d
Claire Tsai <57058281+awhirl420@users.noreply.github.com>
2019-12-31 03:40:30 +0800

    Update L02-Pyhon_Pandas.py

--- Python/Stock/L02-Pyhon_Pandas.py
+++ Python/Stock/L02-Pyhon_Pandas.py
@@ -46,3 +46,31 @@ kg_watch.loc['2018-01-03':] += 5
 %matplotlib inline
 kg_watch.plot()
 
+############# DataFrame
+date = pd.date_range('20180101', periods=4)
+
+s1 = pd.Series([1,2,3,4], index=date)
+s2 = pd.Series([7,8,9,10], index=date)
+s3 = pd.Series([11,12,8,2], index=date)
+
+dictionary = {
+    'c1': s1,
+    'c2': s2,
+    'c3': s3,
+}
+
+df = pd.DataFrame(dictionary)
+df
+
+%matplotlib inline
+df.plot()
+
+# get sepecific data
+df.loc['2018-01-02':'2018-01-04',['c1', 'c2']]
+df.iloc[0:2,[0, 2]]
+df['c3']
+
+print(df)
+df.cumprod(axis=0) # cumulative product by rows
+df.cumprod(axis=1) # cumulative product by columns
+df.drop('c2', axis=1)
