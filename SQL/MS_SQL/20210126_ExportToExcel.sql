f751ba30aff33b81da9ce9c13dfa0f6abfb03224
Claire Tsai <57058281+awhirl420@users.noreply.github.com>
2021-01-26 23:14:21 +0800

    Update 20210126_ExportToExcel.sql

--- SQL/MS_SQL/20210126_ExportToExcel.sql
+++ SQL/MS_SQL/20210126_ExportToExcel.sql
@@ -1,3 +1,12 @@
+--Method1
 --Working with data in Excel or Access requires the appropriate driver:
 --https://www.microsoft.com/en-us/download/details.aspx?id=54920
+
+INSERT INTO OPENROWSET('Microsoft.Jet.OLEDB.4.0',
+'Excel 8.0;Database=C:\Users\WinnieTsai\testing.xls;',
+'SELECT * FROM [Sheet1$]')
+SELECT *, GETDATE() FROM AdventureWorks2017.dbo.CustomerView
+GO
+
+
 
